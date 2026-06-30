6c8de7f44de25b3016695e5c1e59fb34fe87c8c9
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-06-30 23:47:56 +0530

    Time: 11 ms (56.68%), Space: 11.5 MB (71.55%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,54 +1,24 @@
 class Solution {
 public:
     int numSplits(string s) {
-        int n=s.size();
-        vector<bool>left(26,false);
-        vector<int>lPref(n,0);
-        vector<int>rSuff(n,0);
-        lPref[0]=1;
-        left[s[0]-'a']=true;
-        for(int i=1;i<n;i++){
-            int idx=s[i]-'a';
-            if(left[idx]==true){
-                lPref[i]=lPref[i-1];
-            }
-            else{
-                lPref[i]=lPref[i-1]+1;
-            }
-            left[idx]=true;
+        unordered_map<char, int> leftmap;
+        unordered_map<char, int> rightmap;
+        int count = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            rightmap[s[i]]++;
         }
-        for(int num : lPref){
-            cout<<num<<" ";
-            cout<<endl;
-        }
-        cout<<endl;
-        left.clear();
-        left.resize(26, false);
-        rSuff[n-1]=1;
-        left[s[n-1]-'a']=true;
-        for(int i=n-2;i>=0;i--){
-            int idx=s[i]-'a';
-            if(left[idx]==true){
-                rSuff[i]=rSuff[i+1];
+        for (int i = 0; i < n; i++) {
+            leftmap[s[i]]++;
+            rightmap[s[i]]--;
+            if (rightmap[s[i]] == 0) {
+                rightmap.erase(s[i]);
             }
-            else{
-                rSuff[i]=rSuff[i+1]+1;
-            }
-            left[idx]=true;
-        }
-        for(int num : rSuff){
-            cout<<num<<" ";
-            
-        }
-        int count=0;
-        for(int i=0;i<n-1;i++){
-            if(lPref[i]==rSuff[i+1]){
+
+            if (leftmap.size() == rightmap.size()) {
                 count++;
             }
         }
         return count;
-
-
-        
     }
 };
