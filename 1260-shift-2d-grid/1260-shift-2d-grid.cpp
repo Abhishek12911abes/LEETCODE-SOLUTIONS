dccfae03b926ca631bad059d7791bdd515340fb0
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-20 14:11:49 +0530

    Time: 53 ms (5.16%), Space: 46.6 MB (5.16%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
 
-        int n = grid.size();
-        int m = grid[0].size();
-        int size = n * m;
-
-        k %= size;
-
-        vector<int> a;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                a.push_back(grid[i][j]);
+        while (k--) {
+            vector<vector<int>> temp(m, vector<int>(n));
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == m - 1 && j == n - 1) {
+                        temp[0][0] = grid[i][j];
+                    } else if (j == n - 1) {
+                        temp[i + 1][0] = grid[i][j];
+                    } else {
+                        temp[i][j + 1] = grid[i][j];
+                    }
+                }
             }
+            grid = temp;
         }
-
-        vector<int> shift;
-        for(int i = size - k; i < size; i++){
-            shift.push_back(a[i]);
-        }
-
-        vector<int> rest;
-        for(int i = 0; i < size - k; i++){
-            rest.push_back(a[i]);
-        }
-
-        vector<vector<int>> final(n, vector<int>(m));
-
-        int row = 0, col = 0;
-vector<int> ans;
-
-for(int x : shift) ans.push_back(x);
-for(int x : rest) ans.push_back(x);
-
-int idx = 0;
-for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-        final[i][j] = ans[idx++];
-    }
-}
-
-        return final;
+        return grid;
     }
 };
