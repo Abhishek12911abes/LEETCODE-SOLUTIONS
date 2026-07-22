30c62cbd7dee0f58eb41de673d27cd6fc685b3b3
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-22 21:16:59 +0530

    Time: 4 ms (24.24%), Space: 16.3 MB (7.89%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     int n;
-    typedef long long ll;
-    vector<vector<ll>> dp;
-    ll solve(int i , int j , vector<vector<int>>& matrix){
-        if(i>=n || j<0 || i<0 || j>=n){
-            return LLONG_MAX;
-        }
-        if(i==n-1){
-            return matrix[i][j];
-        }
-        if(dp[i][j]!=LLONG_MAX){
-            return dp[i][j];
-        }
-        ll leftDiagonal=LLONG_MAX;
-        ll down=LLONG_MAX;
-        ll rightDiagonal=LLONG_MAX;
-        if(i+1<n && j-1>=0){
-            leftDiagonal=solve(i+1,j-1,matrix);
-        }
-        if(i+1<n){
-            down=solve(i+1,j,matrix);
-        }
-        if(i+1<n && j+1<n){
-            rightDiagonal=solve(i+1,j+1,matrix);
-        }
-        return dp[i][j]=matrix[i][j]+min({leftDiagonal,down,rightDiagonal});
-    }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        n=matrix.size();
-        dp.assign(n,vector<ll>(n,LLONG_MAX));
-        ll minSum=LLONG_MAX;
-        for(int i=0;i<n;i++){
-            minSum=min(minSum,solve(0,i,matrix));
-        }
-        return (int)minSum;
 
-        
+    vector<vector<int>> dp;
+
+    int solve(int i, int j, vector<vector<int>>& matrix) {
+        if (i >= n || j < 0 || j >= n)
+            return INT_MAX / 2;
+
+        if (i == n - 1)
+            return matrix[i][j];
+
+        if (dp[i][j] != INT_MAX)
+            return dp[i][j];
+
+        int leftDiagonal = solve(i + 1, j - 1, matrix);
+        int down = solve(i + 1, j, matrix);
+        int rightDiagonal = solve(i + 1, j + 1, matrix);
+
+        return dp[i][j] = matrix[i][j] +
+                          min({leftDiagonal, down, rightDiagonal});
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        n = matrix.size();
+
+        dp.assign(n, vector<int>(n, INT_MAX));
+
+        int ans = INT_MAX;
+
+        for (int j = 0; j < n; j++) {
+            ans = min(ans, solve(0, j, matrix));
+        }
+
+        return ans;
     }
 };
