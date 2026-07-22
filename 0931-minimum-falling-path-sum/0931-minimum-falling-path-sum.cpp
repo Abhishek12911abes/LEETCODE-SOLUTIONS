4ec6a1553239aa3122dfe1eab2636cb3de235e35
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-22 21:25:50 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,38 +1,68 @@
 class Solution {
 public:
-    int n;
+    // int n;
+    // vector<vector<int>> dp;
 
-    vector<vector<int>> dp;
+    // int solve(int i, int j, vector<vector<int>>& matrix) {
+    //     if (i >= n || j < 0 || j >= n)
+    //         return INT_MAX / 2;
 
-    int solve(int i, int j, vector<vector<int>>& matrix) {
-        if (i >= n || j < 0 || j >= n)
-            return INT_MAX / 2;
+    //     if (i == n - 1)
+    //         return matrix[i][j];
 
-        if (i == n - 1)
-            return matrix[i][j];
+    //     if (dp[i][j] != INT_MAX)
+    //         return dp[i][j];
 
-        if (dp[i][j] != INT_MAX)
-            return dp[i][j];
+    //     int leftDiagonal = solve(i + 1, j - 1, matrix);
+    //     int down = solve(i + 1, j, matrix);
+    //     int rightDiagonal = solve(i + 1, j + 1, matrix);
 
-        int leftDiagonal = solve(i + 1, j - 1, matrix);
-        int down = solve(i + 1, j, matrix);
-        int rightDiagonal = solve(i + 1, j + 1, matrix);
-
-        return dp[i][j] = matrix[i][j] +
-                          min({leftDiagonal, down, rightDiagonal});
-    }
+    //     return dp[i][j] = matrix[i][j] +
+    //                       min({leftDiagonal, down, rightDiagonal});
+    // }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        n = matrix.size();
+        // n = matrix.size();
 
-        dp.assign(n, vector<int>(n, INT_MAX));
+        // // Recursion + Memoization
+
+        // dp.assign(n, vector<int>(n, INT_MAX));
+
+        // int ans = INT_MAX;
+
+        // for (int j = 0; j < n; j++) {
+        //     ans = min(ans, solve(0, j, matrix));
+        // }
+
+        // return ans;
+
+         int n = matrix.size();
+
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        // Base case: last row
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = matrix[n - 1][j];
+        }
+
+        // Fill from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+
+                int leftDiagonal = (j > 0) ? dp[i + 1][j - 1] : INT_MAX / 2;
+                int down = dp[i + 1][j];
+                int rightDiagonal = (j < n - 1) ? dp[i + 1][j + 1] : INT_MAX / 2;
+
+                dp[i][j] = matrix[i][j] + min({leftDiagonal, down, rightDiagonal});
+            }
+        }
 
         int ans = INT_MAX;
-
         for (int j = 0; j < n; j++) {
-            ans = min(ans, solve(0, j, matrix));
+            ans = min(ans, dp[0][j]);
         }
 
         return ans;
+
     }
 };
