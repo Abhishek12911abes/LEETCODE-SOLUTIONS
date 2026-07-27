ec72e44f6eff2999fde43ae83b071956d47e4fc7
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-27 14:34:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (69.6%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // int dp[38];
+    // int solve(int n ){
+    //     if(n==0) return 0;
+    //     if(n==1 || n==2) return 1;
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]=solve(n-1)+solve(n-2)+solve(n-3);
+    // }
+    int tribonacci(int n) {
+        // Brute-> Recursion + Memoization
+
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        // memset(dp,-1,sizeof(dp));
+        // return solve(n);
+
+         int dp[38];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        return dp[n];
+        
+    }
+};
