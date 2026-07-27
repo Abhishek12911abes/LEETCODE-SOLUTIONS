6e72bae343bf562ca2f0809b419cc998a040ee1b
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-27 15:42:59 +0530

    Time: 0 ms (100%), Space: 10.7 MB (52.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int n;
-    int t[1001];
-    int solve(int idx , vector<int>& nums , int n){
-        if(idx>n){
-            return 0;
-        }
+    // int t[1001];
+    // int solve(int idx , vector<int>& nums , int n){
+    //     if(idx>n){
+    //         return 0;
+    //     }
 
-        if(t[idx]!=-1){
-            return t[idx];
-        }
+    //     if(t[idx]!=-1){
+    //         return t[idx];
+    //     }
 
-        int rob=nums[idx]+solve(idx+2,nums,n);
-        int skip=solve(idx+1,nums,n);
+    //     int rob=nums[idx]+solve(idx+2,nums,n);
+    //     int skip=solve(idx+1,nums,n);
 
-        return t[idx]=max(rob,skip);
-    }
+    //     return t[idx]=max(rob,skip);
+    // }
     int rob(vector<int>& nums) {
         n=nums.size();
 
@@ -23,16 +23,50 @@ public:
             return nums[0];
         }
 
-        if(n==2){
-            return max(nums[0],nums[1]);
-        }
-        memset(t,-1,sizeof(t));
-        int idx0=solve(0,nums,n-2);
+        // if(n==2){
+        //     return max(nums[0],nums[1]);
+        // }
+        // memset(t,-1,sizeof(t));
+        // int idx0=solve(0,nums,n-2);
 
-        memset(t,-1,sizeof(t));
-        int idx1=solve(1,nums,n-1);
+        // memset(t,-1,sizeof(t));
+        // int idx1=solve(1,nums,n-1);
 
-        return max(idx0,idx1);
+        // return max(idx0,idx1);
+
+        vector<int>dp(n+1,0);
         
+        // Case 1 ) Take 1st House and skip last House
+
+        // State Defn -> dp[i]=money stolen when u have i houses;
+
+        dp[0]=0;
+
+        for(int i=1;i<=n-1;i++){
+            int skip=dp[i-1];
+            int rob=nums[i-1]+((i-2>=0)?dp[i-2]:0);
+
+            dp[i]=max(skip,rob);
+        }
+        int res1=dp[n-1];
+
+        dp.clear();
+
+        //Case 2 ) Skip 1st House and hence take last house;
+
+        dp[0]=0;
+
+        dp[1]=0; // i have skipped first house;
+
+        for(int i=2;i<=n;i++){
+            int skip=dp[i-1];
+            int rob=nums[i-1]+((i-2>=0)?dp[i-2]:0);
+
+            dp[i]=max(skip,rob);
+        }
+        int res2=dp[n];
+
+        return max(res1,res2);
     }
+
 };
