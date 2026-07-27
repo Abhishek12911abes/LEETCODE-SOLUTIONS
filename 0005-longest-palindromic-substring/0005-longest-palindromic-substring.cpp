dddb2c6bbb76eadf5c6e760bc39e85bcb16b2a96
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-27 16:09:16 +0530

    Time: 8 ms (69.31%), Space: 9.5 MB (50.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,66 @@
 class Solution {
 public:
+    int start_i=-1;
+    int maxLen=-1;
+    void expand(int i , int j , string& s){
+
+
+            while(i>=0 && j<s.size() && s[i]==s[j]){
+                if(j-i+1>maxLen){
+                    maxLen=j-i+1;
+                    start_i=i;
+                }
+                i--,j++;
+            }
+            
+        }
     string longestPalindrome(string s) {
 
         int n=s.size();
         // Approach 1 ) DP table
-        vector<vector<bool>>dp(n,vector<bool>(n));
+        // vector<vector<bool>>dp(n,vector<bool>(n));
+
+        // for(int i=0;i<n;i++){
+        //     dp[i][i]=true;
+        // }
+
+        // for(int L=2;L<=n;L++){
+        //     for(int i=0;i+L-1<n;i++){
+        //         int j=i+L-1;
+        //         if(L==2){
+        //             if(s[i]==s[j]){
+        //                 dp[i][j]=true;
+        //             }
+        //         }
+        //         else{
+        //             if(s[i]==s[j] && dp[i+1][j-1]==true){
+        //                 dp[i][j]=true;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // int start_i=-1;
+        // int maxLen=-1;
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(dp[i][j]){
+        //             if(j-i+1>maxLen){
+        //                 maxLen=j-i+1;
+        //                 start_i=i;
+        //             }
+        //         }
+        //     }
+        // }
+        // return s.substr(start_i,maxLen);
+
+        // Approach 2 ) Using Center Expansion
+
 
         for(int i=0;i<n;i++){
-            dp[i][i]=true;
-        }
-
-        for(int L=2;L<=n;L++){
-            for(int i=0;i+L-1<n;i++){
-                int j=i+L-1;
-                if(L==2){
-                    if(s[i]==s[j]){
-                        dp[i][j]=true;
-                    }
-                }
-                else{
-                    if(s[i]==s[j] && dp[i+1][j-1]==true){
-                        dp[i][j]=true;
-                    }
-                }
-            }
-        }
-
-        int start_i=-1;
-        int maxLen=-1;
-
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(dp[i][j]){
-                    if(j-i+1>maxLen){
-                        maxLen=j-i+1;
-                        start_i=i;
-                    }
-                }
-            }
+            expand(i,i,s);
+            expand(i,i+1,s);
         }
         return s.substr(start_i,maxLen);
         
