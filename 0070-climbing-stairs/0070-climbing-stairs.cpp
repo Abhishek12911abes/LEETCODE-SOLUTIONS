f3add3ba8bee2a087cc870453f30cf9b4d47c323
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-07-27 14:07:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    int dp[46];
-    int solve(int n){
-        if(n<0){
-            return 0;
-        }
-        if(n==0){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int oneStep=solve(n-1);
-        int twoStep=solve(n-2);
+    // int dp[46];
+    // int solve(int n){
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int oneStep=solve(n-1);
+    //     int twoStep=solve(n-2);
 
-        return dp[n]=oneStep+twoStep;
-    }
+    //     return dp[n]=oneStep+twoStep;
+    // }
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(n);
+
+        vector<int> dp(n + 1);
+        if (n == 0)
+            return 0;
+        dp[0] = 0;
+        if (n == 1)
+            return 1;
+        dp[1] = 1;
+        if (n == 2)
+            return 2;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
