5ee92cf470483d5af8773dac6c4edd93e406f9e5
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-02 20:07:40 +0530

    Time: 11 ms (27.05%), Space: 11.8 MB (34.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
+    int dp[501][501];
+    int solve(vector<int>& nums , int i , int j){
+        if(i>j){
+            return 0;
+        }
+        if(i==j){
+            return nums[i];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int ith_idx=nums[i]-solve(nums,i+1,j);
+        int jth_idx=nums[j]-solve(nums,i,j-1);
+
+        return  dp[i][j]=max(ith_idx,jth_idx);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
-        
+        int n=piles.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(piles,0,n-1)>=0;
+
     }
 };
