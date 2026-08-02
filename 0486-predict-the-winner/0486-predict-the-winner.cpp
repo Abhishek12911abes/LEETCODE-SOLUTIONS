03bd2961e998669f8fc3534c7d863da018b34397
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-02 19:30:20 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.53%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     // Pattern -> Min Max Dp
+    // int solve(vector<int>& nums , int i , int j){
+    //     if(i>j){
+    //         return 0;
+    //     }
+    //     if(i==j){
+    //         return nums[i];
+    //     }
+    //     int ithIdxTake=nums[i]+min(solve(nums,i+2,j),solve(nums,i+1,j-1));
+    //     int jthIdxTake=nums[j]+min(solve(nums,i,j-2),solve(nums,i+1,j-1));
+
+    //     return max(ithIdxTake,jthIdxTake);
+    // }
+    int dp[21][21];
     int solve(vector<int>& nums , int i , int j){
         if(i>j){
             return 0;
@@ -8,18 +21,25 @@ public:
         if(i==j){
             return nums[i];
         }
-        int ithIdxTake=nums[i]+min(solve(nums,i+2,j),solve(nums,i+1,j-1));
-        int jthIdxTake=nums[j]+min(solve(nums,i,j-2),solve(nums,i+1,j-1));
 
-        return max(ithIdxTake,jthIdxTake);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int take_ith=nums[i]-solve(nums,i+1,j);
+        int take_jth=nums[j]-solve(nums,i,j-1);
+
+        return dp[i][j]=max(take_ith,take_jth);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        int totalScore=accumulate(begin(nums),end(nums),0);
-        int P1Score=solve(nums,0,n-1);
-        int P2Score=totalScore-P1Score;
+        // int totalScore=accumulate(begin(nums),end(nums),0);
+        // int P1Score=solve(nums,0,n-1);
+        // int P2Score=totalScore-P1Score;
 
-        return P1Score-P2Score>=0;
+        // return P1Score-P2Score>=0;
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0,n-1)>=0;
         
     }
 };
