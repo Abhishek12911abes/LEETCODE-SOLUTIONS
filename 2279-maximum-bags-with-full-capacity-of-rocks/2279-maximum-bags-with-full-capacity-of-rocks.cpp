76e01dab39e726d413e29be180a4dbca4c08c440
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-04 14:07:04 +0530

    Time: 24 ms (27.87%), Space: 91.1 MB (66.39%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int maximumBags(vector<int>& capacity, vector<int>& rocks,int additionalRocks) {
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+
         int n = capacity.size();
 
-        vector<pair<int, int>> v(n);
+        vector<int> need(n);
+
         for (int i = 0; i < n; i++) {
-            v[i] = {capacity[i], rocks[i]};
+            need[i] = capacity[i] - rocks[i];
         }
-        sort(v.begin(), v.end(), [](auto& a, auto& b) {
-            return (a.first - a.second) < (b.first - b.second);
-        });
+
+        sort(need.begin(), need.end());
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i].first == v[i].second) {
-                count++;
-                continue;
-            } else if (v[i].first > v[i].second + additionalRocks) {
-                continue;
-            } else {
-                int need = v[i].first - v[i].second;
-                additionalRocks -= need;
+
+        for (int x : need) {
+            if (x == 0) {
                 count++;
             }
+            else if (additionalRocks >= x) {
+                additionalRocks -= x;
+                count++;
+            }
+            else {
+                break;
+            }
         }
+
         return count;
     }
 };
