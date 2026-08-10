0e7bba4e6d38ccc42825fcf1a1df9a3d157c0f3e
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-10 12:22:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.28%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,37 +3,65 @@
 //S.C : O(101) ~= O(1)
 class Solution {
 public:
-    int t[101];
-    int solve(int i, string &s, int &n) {
-        if(t[i] != -1) {
-            return t[i];
-        }
+    // int t[101];
+    // int solve(int i, string &s, int &n) {
+    //     if(t[i] != -1) {
+    //         return t[i];
+    //     }
 
-        if(i == n) {
-            return t[i] = 1; //one valid split done
-        }
+    //     if(i == n) {
+    //         return t[i] = 1; //one valid split done
+    //     }
 
-        if(s[i] == '0') {
-            return t[i] = 0; //not possible to split
-        }
+    //     if(s[i] == '0') {
+    //         return t[i] = 0; //not possible to split
+    //     }
 
-        int result     = solve(i+1, s, n);
+    //     int result     = solve(i+1, s, n);
         
-        if(i+1 < n) {
-            if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
-                result += solve(i+2, s, n);
-        }
+    //     if(i+1 < n) {
+    //         if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
+    //             result += solve(i+2, s, n);
+    //     }
 
         
-        return t[i] = result;
+    //     return t[i] = result;
 
-    }
+    // }
 
     int numDecodings(string s) {
         int n = s.length();
         
-        memset(t, -1, sizeof(t));
-        return solve(0, s, n);
+        // memset(t, -1, sizeof(t));
+        // return solve(0, s, n);
+
+
+        // Approach-2 (Tabulation)
+
+        vector<int>dp(n+1);
+
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+            }
+            else{
+                // Single digit
+                dp[i] = dp[i + 1];
+
+                // Two digit
+                if(i + 1 < n &&
+                   (s[i] == '1' ||
+                   (s[i] == '2' && s[i + 1] <= '6'))) {
+
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
+
+
+
         
 
     }
