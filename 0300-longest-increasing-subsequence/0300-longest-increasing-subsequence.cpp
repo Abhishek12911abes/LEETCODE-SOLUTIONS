ff6d9277456509be5cc33e02c016510540c8fef3
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-11 16:36:11 +0530

    Time: 47 ms (72.11%), Space: 14.4 MB (48.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    int n;
-    int dp[2501][2501];
-    int solve(vector<int>& nums , int currIdx , int prevIdx){
-        if(currIdx>=n){
-            return 0;
-        }
-        if(dp[currIdx][prevIdx+1]!=-1){
-            return dp[currIdx][prevIdx+1];
-        }
-        int take=0,skip=0;
+    // int n;
+    // int dp[2501][2501];
+    // int solve(vector<int>& nums , int currIdx , int prevIdx){
+    //     if(currIdx>=n){
+    //         return 0;
+    //     }
+    //     if(dp[currIdx][prevIdx+1]!=-1){
+    //         return dp[currIdx][prevIdx+1];
+    //     }
+    //     int take=0,skip=0;
 
-        skip=solve(nums,currIdx+1,prevIdx);
+    //     skip=solve(nums,currIdx+1,prevIdx);
 
-        if(prevIdx==-1 || nums[currIdx]>nums[prevIdx]){
-            take=1+solve(nums,currIdx+1,currIdx);
-        }
+    //     if(prevIdx==-1 || nums[currIdx]>nums[prevIdx]){
+    //         take=1+solve(nums,currIdx+1,currIdx);
+    //     }
 
-        return dp[currIdx][prevIdx+1]=max(take,skip);
-    }
+    //     return dp[currIdx][prevIdx+1]=max(take,skip);
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
+        int n=nums.size();
 
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
 
-        return solve(nums,0,-1);
+        // return solve(nums,0,-1);
+
+        vector<int>dp(n+1,1);
+
+        int maxLen=1;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    maxLen=max(maxLen,dp[i]);
+                }
+            }
+        }
+        return maxLen;
         
     }
 };
