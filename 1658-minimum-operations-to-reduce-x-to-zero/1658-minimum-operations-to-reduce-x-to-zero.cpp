c5584e8f0815c2fd9147b53bf9b571235c926bb1
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-17 03:09:56 +0530

    Time: 0 ms (100%), Space: 102.2 MB (91.95%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,38 +1,79 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums, int x) {
+//         int n = nums.size();
+
+//         int totalSum = 0;
+//         for (int num : nums) {
+//             totalSum += num;
+//         }
+
+//         int target = totalSum - x;
+
+//         // If we need to keep an empty subarray
+//         if (target == 0) return n;
+
+//         unordered_map<int, int> mp;
+
+//         // prefixSum -> index
+//         mp[0] = -1;
+
+//         int sum = 0;
+//         int maxLen = -1;
+
+//         for (int j = 0; j < n; j++) {
+//             sum += nums[j];
+
+//             int needed = sum - target;
+
+//             if (mp.count(needed)) {
+//                 maxLen = max(maxLen, j - mp[needed]);
+//             }
+
+//             // Store first occurrence only
+//             if (!mp.count(sum)) {
+//                 mp[sum] = j;
+//             }
+//         }
+
+//         return maxLen == -1 ? -1 : n - maxLen;
+//     }
+// };
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
 
-        int totalSum = 0;
+        int total = 0;
+
         for (int num : nums) {
-            totalSum += num;
+            total += num;
         }
 
-        int target = totalSum - x;
+        int target = total - x;
 
-        // If we need to keep an empty subarray
-        if (target == 0) return n;
+        if (target == 0)
+            return n;
 
-        unordered_map<int, int> mp;
+        if (target < 0)
+            return -1;
 
-        // prefixSum -> index
-        mp[0] = -1;
-
+        int i = 0;
         int sum = 0;
         int maxLen = -1;
 
         for (int j = 0; j < n; j++) {
+
             sum += nums[j];
 
-            int needed = sum - target;
-
-            if (mp.count(needed)) {
-                maxLen = max(maxLen, j - mp[needed]);
+            while (sum > target && i <= j) {
+                sum -= nums[i];
+                i++;
             }
 
-            // Store first occurrence only
-            if (!mp.count(sum)) {
-                mp[sum] = j;
+            if (sum == target) {
+                maxLen = max(maxLen, j - i + 1);
             }
         }
 
