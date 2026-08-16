40d6e05c57d450f7607ad6162c41803b9495bc29
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-16 12:48:09 +0530

    Time: 54 ms (6.76%), Space: 25.6 MB (40.15%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,11 +1,54 @@
-//Approach-1 (Sliding Window + modifying input s = s+s)
+// //Approach-1 (Sliding Window + modifying input s = s+s)
+// //T.C : O(2*n) ~= O(n)
+// //S.C : O(2*n) for s1 and s2 and s+=s
+// class Solution {
+// public:
+//     int minFlips(string s) {
+//         int n = s.size();
+//         s += s;
+
+//         string s1, s2;
+
+//         for(int i = 0; i < 2*n; i++) {
+//             s1 += (i % 2 ? '0' : '1');
+//             s2 += (i % 2 ? '1' : '0');
+//         }
+
+//         int i = 0, j = 0;
+//         int result1 = 0, result2 = 0;
+//         int result = INT_MAX;
+
+//         while(j < 2*n) {
+
+//             // expand window
+//             if(s[j] != s1[j]) result1++;
+//             if(s[j] != s2[j]) result2++;
+
+//             // shrink if window exceeds n
+//             if(j - i + 1 > n) {
+//                 if(s[i] != s1[i]) result1--;
+//                 if(s[i] != s2[i]) result2--;
+//                 i++;
+//             }
+
+//             // window size exactly n
+//             if(j - i + 1 == n)
+//                 result = min({result, result1, result2});
+
+//             j++;
+//         }
+
+//         return result;
+//     }
+// };
+
+//Approach-2 (Sliding Window + without modifying input)
 //T.C : O(2*n) ~= O(n)
-//S.C : O(2*n) for s1 and s2 and s+=s
+//S.C : O(2*n) for s1 and s2
 class Solution {
 public:
     int minFlips(string s) {
         int n = s.size();
-        s += s;
 
         string s1, s2;
 
@@ -19,15 +62,19 @@ public:
         int result = INT_MAX;
 
         while(j < 2*n) {
-
+            
             // expand window
-            if(s[j] != s1[j]) result1++;
-            if(s[j] != s2[j]) result2++;
+            if(s[j%n] != s1[j]) 
+                result1++;
+            if(s[j%n] != s2[j]) 
+                result2++;
 
             // shrink if window exceeds n
             if(j - i + 1 > n) {
-                if(s[i] != s1[i]) result1--;
-                if(s[i] != s2[i]) result2--;
+                if(s[i%n] != s1[i]) 
+                    result1--;
+                if(s[i%n] != s2[i]) 
+                    result2--;
                 i++;
             }
 
@@ -41,3 +88,4 @@ public:
         return result;
     }
 };
+
