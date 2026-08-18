ee749035503997c738e4995d93558bbe0e1fe448
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-19 00:22:42 +0530

    Time: 88 ms (20.4%), Space: 130.8 MB (29.18%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
@@ -11,26 +11,48 @@ public:
         }   
 
         if(sum1>sum2){
-            priority_queue<int>pq;
+            // ab dekh sum1>sum2 matlab mujhe sum1 ko kam krna hoga aur sum2 ko inc krna hoga 
+
+            //ab nums1 k har elem ko mai chahunga ki wo kam se kam 1 bane taki usse hamara sum kam ho ske . for eg agar num=6 hai toh usko 1 banao taki sum 5 se kam ho ske isliye (num-1) for eg num=5 hai toh usko bhi 1 banao taki sum 4 se kam ho ske isliye again(num-1)
+
+            priority_queue<int>pq; // pq kyu liya hu wo aage smjh aaega
+
             for(int num : nums1){
                 pq.push(num-1);
             }
+
+            // ab baari hai sum2 ki mujhe sum2 ko inc krna hoga ab mai chahunga ki har elem ko 6 banau taki zyada se zyada sum ho ske. 
+
+            //isko kaise kre --> maan le num 1 hai toh usko 6 banau taki sum 5 se inc ho ske 
+            // ab ye hoga kaise ?? ---> (6-num) krega toh har elem ...sum ko kuch na kuch inc krega
 
             for(int num : nums2){
                 pq.push(6-num);
             }
 
+            // ab mere nums1 ka har elem jo ki mere sum1 ko kitna dec kr skta hai usko pq(maxheap) me daala hu
+
+            // same for har elem nums2 ke sum2 ko kitna inc kr skta h usko pq(maxheap) me daala hu
+
+            // maxheap se ye faida hoga wo ye check krega ki kaha se zyada faida mil rha hai  ---> sum1 ko dec krne me ya sum2 ko inc krne me kyuki pq ke top pe max val hoga
+
+
+
+             
+           
             int ops=0;
             int diff=sum1-sum2;
             
-            while(!pq.empty() && diff>0){
+            while(!pq.empty() && diff>0){ // jabtak mera diff 0 se zyada hai
                 int maxi=pq.top();
                 pq.pop();
                 diff-=maxi;
                 ops++;
             }
-            return diff<=0?ops:-1;
+            return diff<=0?ops:-1; // agar in future mai kabhi dono ke sum ko equal nhi kr paya toh -1;
         }
+
+        // ab ye pura logic rev hoga for sum1<sum2 ke liye
         else{
             priority_queue<int>pq;
             for(int num : nums1){
@@ -50,7 +72,7 @@ public:
             }
             return diff<=0?ops:-1;
         }
-        return -1;
+        return -1; // dummy statement
         
     }
 };
