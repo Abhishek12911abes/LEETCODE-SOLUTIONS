bac28b1a96150cea7fdaf3a1245e5988f449d444
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-19 16:37:37 +0530

    Time: 592 ms (81.14%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
 select tweet_id
 from tweets
-where LENGTH(content)>15;
+where CHAR_LENGTH(content)>15;
+
+-- Length fun in sql string ki len nikalta hai bytes me
+-- Char_length fun in sql string ki length nikalta hai Char me
