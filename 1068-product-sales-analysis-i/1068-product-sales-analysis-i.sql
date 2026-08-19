1a343c0824a16a0b97f0d8554a1f98d13db3f4d5
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-19 17:10:56 +0530

    Time: 1497 ms (25.79%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -2,4 +2,10 @@
 Select p.product_name as product_name , s.year as year , s.price as price
 From Sales as s
 left join product as p
-on s.product_id=p.product_id;
+-- on s.product_id=p.product_id;
+
+-- SQL me USING ka use JOIN ke time hota hai jab dono tables me same naam ka column ho jiske basis par join karna hai.
+
+Using(product_id);
+
+
