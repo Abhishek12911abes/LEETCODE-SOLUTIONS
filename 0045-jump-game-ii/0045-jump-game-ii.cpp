374606f8c7ff161fda3acbd29d540fcf96f9bc2a
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-23 01:08:45 +0530

    Time: 171 ms (12.25%), Space: 21.7 MB (23.6%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[10001];
+    int solve(vector<int>& nums, int n, long long idx) {
+        if (idx == n - 1) {
+            return 0;
+        }
+        if (idx >= n) {
+            return INT_MAX;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int minMoves = INT_MAX;
+        for (int i = 1; i <= nums[idx]; i++) {
+            int moves = solve(nums, n, idx + i);
+            if (moves != INT_MAX) {
+                minMoves = min(minMoves, 1 + moves);
+            }
+        }
+        return dp[idx]=minMoves;
+    }
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(nums, n, 0);
+    }
+};
