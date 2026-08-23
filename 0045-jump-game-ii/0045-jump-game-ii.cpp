8d8e58159a0dfc2c2a5981c01ef6ab727f847e5e
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-23 12:24:20 +0530

    Time: 497 ms (5.21%), Space: 21.6 MB (25.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    int dp[10001];
-    int solve(vector<int>& nums, int n, long long idx) {
-        if (idx == n - 1) {
-            return 0;
-        }
-        if (idx >= n) {
-            return INT_MAX;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int minMoves = INT_MAX;
-        for (int i = 1; i <= nums[idx]; i++) {
-            int moves = solve(nums, n, idx + i);
-            if (moves != INT_MAX) {
-                minMoves = min(minMoves, 1 + moves);
-            }
-        }
-        return dp[idx]=minMoves;
-    }
+    // int dp[10001];
+    // int solve(vector<int>& nums, int n, long long idx) {
+    //     if (idx == n - 1) {
+    //         return 0;
+    //     }
+    //     if (idx >= n) {
+    //         return INT_MAX;
+    //     }
+    //     if(dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
+    //     int minMoves = INT_MAX;
+    //     for (int i = 1; i <= nums[idx]; i++) {
+    //         int moves = solve(nums, n, idx + i);
+    //         if (moves != INT_MAX) {
+    //             minMoves = min(minMoves, 1 + moves);
+    //         }
+    //     }
+    //     return dp[idx]=minMoves;
+    // }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums, n, 0);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums, n, 0);
+
+        // Bottom Up (Tabulation)
+
+        // State defn - dp[i]=min number of steps to move from index 0 to index i;
+        vector<int>dp(n,INT_MAX);
+        dp[0]=0;
+
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(j+nums[j]>=i){
+                    dp[i]=min(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return dp[n-1];
+
     }
 };
