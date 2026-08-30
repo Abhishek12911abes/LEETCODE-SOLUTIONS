c0b1dd41b5a47047cc2f943c965cebb3fda8642b
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-08-30 21:17:33 +0530

    Time: 19 ms (9.43%), Space: 11.6 MB (28.04%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    int n;
     int dp[501][501];
     int solve(vector<int>& nums , int i , int j){
         if(i>j){
             return 0;
         }
-        if(i==j){
-            return nums[i];
-        }
-
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
+        int op1=nums[i]+solve(nums,i+1,j);
+        int op2=nums[j]+solve(nums,i,j-1);
 
-        int ith_idx=nums[i]-solve(nums,i+1,j);
-        int jth_idx=nums[j]-solve(nums,i,j-1);
+        int maxi1=max(op1,op2);
 
-        return  dp[i][j]=max(ith_idx,jth_idx);
+        int op3=nums[i]+solve(nums,i+2,j);
+        int op4=nums[j]+solve(nums,i,j-2);
+
+        int maxi2=max(op3,op4);
+
+        return dp[i][j]=max(maxi1,maxi2);
     }
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
+        n=piles.size();
+        int totalPiles=accumulate(begin(piles),end(piles),0);
         memset(dp,-1,sizeof(dp));
-        return solve(piles,0,n-1)>=0;
-
+        return (totalPiles-solve(piles,0,n-1))>=0;
+        
     }
 };
