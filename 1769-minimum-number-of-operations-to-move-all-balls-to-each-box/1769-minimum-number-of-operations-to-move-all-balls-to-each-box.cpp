0ee93394448de7dba4d933a931825302fe12b65f
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-02 12:37:52 +0530

    Time: 0 ms (100%), Space: 12.1 MB (85.23%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,22 +2,53 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            int minOps=0;
-            for(int k=0;k<i;k++){
-                if(boxes[k]=='1'){
-                    minOps+=abs(k-i);
-                }
-            }
-            for(int j=i+1;j<n;j++){
-                if(boxes[j]=='1'){
-                    minOps+=abs(j-i);
-                }
-            }
-            ans.push_back(minOps);
+        // for(int i=0;i<n;i++){
+        //     int minOps=0;
+        //     for(int k=0;k<i;k++){
+        //         if(boxes[k]=='1'){
+        //             minOps+=abs(k-i);
+        //         }
+        //     }
+        //     for(int j=i+1;j<n;j++){
+        //         if(boxes[j]=='1'){
+        //             minOps+=abs(j-i);
+        //         }
+        //     }
+        //     ans.push_back(minOps);
+        // }
+        // return ans;
+
+        vector<int> answer(n, 0);
+
+        int cumValue    = 0;
+        int cumValueSum = 0;
+
+        //Find moves for all left balls to index i
+        //O(n)
+        for(int i = 0; i < n; i++) {
+            answer[i] = cumValueSum;
+            
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            
+            cumValueSum += cumValue;
         }
-        return ans;
+
+        cumValue    = 0;
+        cumValueSum = 0;
+        //Find moves for all right balls to index i
+        //O(n)
+        for(int i = n-1; i >= 0; i--) {
+            answer[i] += cumValueSum;
+            
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            
+            cumValueSum += cumValue;
+        }
+
+        //O(n+n) ~ O(n)
+        return answer;
+
+        
         
         
     }
