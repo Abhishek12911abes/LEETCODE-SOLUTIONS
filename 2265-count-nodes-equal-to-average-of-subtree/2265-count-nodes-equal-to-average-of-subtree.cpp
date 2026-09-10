9d9705b41f4266b42f17765efebeb6055ce90419
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-10 16:26:24 +0530

    Time: 3 ms (91.06%), Space: 15.9 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,39 +11,67 @@
  */
 class Solution {
 public:
-    int findSum(TreeNode* root , int &count){
+    // int findSum(TreeNode* root , int &count){
+    //     if(root==NULL){
+    //         return 0;
+    //     }
+
+    //     count++;
+
+    //     int lSum=findSum(root->left,count);
+    //     int rSum=findSum(root->right,count);
+
+    //     return lSum+rSum+root->val;
+    // }
+    // int solve(TreeNode* root){
+    //     if(root==NULL){
+    //         return 0;
+    //     }
+    //     int count=0;
+    //     int res=0;
+
+    //     int sum=findSum(root,count);
+
+    //     if(root->val==sum/count){
+    //         res++;
+    //     }
+
+    //     res+=solve(root->left);
+    //     res+=solve(root->right);
+
+    //     return res;
+
+    // }
+    int result;
+    pair<int,int> solve(TreeNode* root){
         if(root==NULL){
-            return 0;
+            return {0,0};
+        }
+        auto p1=solve(root->left);
+        auto p2=solve(root->right);
+
+        int totalSum=p1.first+p2.first+root->val;
+
+        int totalNodes=p1.second+p2.second+1;
+
+        if((totalSum/totalNodes)==root->val){
+            result+=1;
         }
 
-        count++;
-
-        int lSum=findSum(root->left,count);
-        int rSum=findSum(root->right,count);
-
-        return lSum+rSum+root->val;
-    }
-    int solve(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-        int count=0;
-        int res=0;
-
-        int sum=findSum(root,count);
-
-        if(root->val==sum/count){
-            res++;
-        }
-
-        res+=solve(root->left);
-        res+=solve(root->right);
-
-        return res;
+        return {totalSum,totalNodes};
 
     }
     int averageOfSubtree(TreeNode* root) {
-        return solve(root);
+        // Approach 1) Brute Force
+
+        // return solve(root);
+
+        // Approach 2) Optimal
+
+        result=0;
+        solve(root);
+
+        return result;
         
     }
 };
