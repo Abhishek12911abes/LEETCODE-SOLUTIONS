264db30439496a7d75eacf4ead041f789325e1b5
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-15 20:23:03 +0530

    Time: 287 ms (20.15%), Space: 19.7 MB (32.72%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,104 @@
+
+//Approach-5 (using different recursion style + Using BluePrint for solving palindromic DP problems as above for O(1) isPalindrome)
+//T.C : O(n^2)
+//S.C : O(n^2)
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    vector<int> t;
+
+    int solve(int n, int k) {
+        if (n < k) return 0;
+
+        if (t[n] != -1) 
+            return t[n];
+
+        int result = solve(n - 1, k); //ignore the current character s[n-1]
+
+        int j = n - 1;
+        //end the current palindrome at the current character s[n-1]
+        for (int i = 0; j-i+1>=k; i++) {
+            if (isPalindrome[i][j]) {
+                result = max(result, 1 + solve(i, k));
+            }
+        }
+
+        return t[n] = result;
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.length();
+        isPalindrome.assign(n, vector<bool>(n, false));
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    isPalindrome[i][i] = true;
+                } else if (i + 1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
+
+        t.assign(n + 1, -1);
+        return solve(n, k);
+    }
+};
+
+
+//Approach-6 (Bottom-up version of Approach-5 above)
+//T.C : O(n^2)
+//S.C : O(n^2)
+// class Solution {
+// public:
+//     vector<vector<bool>> isPalindrome;
+//     vector<int> t;
+
+//     int maxPalindromes(string s, int k) {
+//         int n = s.length();
+//         isPalindrome.assign(n, vector<bool>(n, false));
+
+//         for (int L = 1; L <= n; L++) {
+//             for (int i = 0; i + L <= n; i++) {
+//                 int j = i + L - 1;
+
+//                 if (i == j) {
+//                     isPalindrome[i][i] = true;
+//                 } else if (i + 1 == j) {
+//                     isPalindrome[i][j] = (s[i] == s[j]);
+//                 } else {
+//                     isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+//                 }
+//             }
+//         }
+
+//         t.assign(n + 1, -1);
+//         //base case
+//         for(int len = 0; len < k; len++) {
+//             t[len] = 0; // if (n < k) return 0;
+//         }
+
+//         for (int len = k; len <= n; len++) {
+
+//             int result = t[len - 1];      // int result = solve(n - 1, k);
+
+//             int j = len - 1;
+//             for (int i = 0; j-i+1 >= k; i++) {
+//                 if (isPalindrome[i][j]) {
+//                     result = max(result, 1 + t[i]);   // result = max(result, 1 + solve(i, k));
+//                 }
+//             }
+
+//             t[len] = result;              // return t[n] = result;
+//         }
+
+//         return t[n];                      // return solve(n, k);
+//     }
+// };
+
+
+
