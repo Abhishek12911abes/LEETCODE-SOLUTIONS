2ebb166f6ed2b46a15f33c4ce14115c11e9f90de
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-20 21:18:55 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.22%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,26 +11,58 @@
  */
 class Solution {
 public:
+    // Brute wala Solve
+
+    // int solve(TreeNode* root){
+    //     if(!root){
+    //         return 0;
+    //     }
+    //     int leftH=solve(root->left);
+    //     int rightH=solve(root->right);
+
+    //     return max(leftH,rightH)+1;
+    // }
+
+    // Optimal wala solve
     int solve(TreeNode* root){
         if(!root){
             return 0;
         }
         int leftH=solve(root->left);
+        if(leftH==-1){
+            return -1;
+        }
+
         int rightH=solve(root->right);
+
+        if(rightH==-1){
+            return -1;
+        }
+
+        if(abs(leftH-rightH)>1){
+            return -1;
+        }
 
         return max(leftH,rightH)+1;
     }
     bool isBalanced(TreeNode* root){
+        //Approach 1) Brute TC O(n^2)
+        // if(!root){
+        //     return true;
+        // }
+        // int leftHeight=solve(root->left);
+        // int rightHeight=solve(root->right);
+
+        // if(abs(leftHeight-rightHeight)>1){
+        //     return false;
+        // }
+
+        // return isBalanced(root->left) && isBalanced(root->right);
+
         if(!root){
             return true;
         }
-        int leftHeight=solve(root->left);
-        int rightHeight=solve(root->right);
 
-        if(abs(leftHeight-rightHeight)>1){
-            return false;
-        }
-
-        return isBalanced(root->left) && isBalanced(root->right);
+        return solve(root)!=-1;
     }
 };
