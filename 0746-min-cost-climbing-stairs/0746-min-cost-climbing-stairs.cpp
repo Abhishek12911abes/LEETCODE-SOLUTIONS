6ca492c62934ba318d964b3071cb67e5274d0200
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-24 18:25:10 +0530

    Time: 0 ms (100%), Space: 17.4 MB (90.33%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int n;
-    int t[1000];
+    int dp[1001];
     int solve(vector<int>& cost , int idx){
         if(idx>=n){
             return 0;
         }
-        if(t[idx]!=-1){
-            return t[idx];
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
-
         int oneStep=cost[idx]+solve(cost,idx+1);
         int twoStep=cost[idx]+solve(cost,idx+2);
 
-        return t[idx]=min(oneStep,twoStep);
+        return dp[idx]=min(oneStep,twoStep);
     }
     int minCostClimbingStairs(vector<int>& cost) {
         n=cost.size();
-        memset(t,-1,sizeof(t));
-        int zeroIndexCost=solve(cost,0);
-        int oneIndexCost=solve(cost,1);
 
-        return min(zeroIndexCost,oneIndexCost);
+        memset(dp,-1,sizeof(dp));
+        
+        int idx0=solve(cost,0);
+
+        int idx1=solve(cost,1);
+
+        return min(idx0,idx1);
         
     }
 };
