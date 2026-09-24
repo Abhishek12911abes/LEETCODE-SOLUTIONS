faf5320667093ed95255aaa097b4651b1d12c1a2
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-24 19:33:12 +0530

    Time: 44 ms (6.56%), Space: 106.8 MB (5.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int n;
-    int dp[100005][2][2];
-    int solve(vector<int>& prices, int idx, int buy, int cap) {
-
-        if (idx >= n || cap == 0) {
+    int dp[100001][2][2];
+    int solve(vector<int>& prices , int idx , int buy , int maxLimit){
+        if(idx>=n){
             return 0;
         }
 
-        if (dp[idx][buy][cap] != -1) {
-            return dp[idx][buy][cap];
+        if(maxLimit==0){
+            return 0;
         }
 
-        int take, skip;
-
-        if (buy) {
-            // Buy
-            take = -prices[idx] + solve(prices, idx + 1, 0, cap);
-
-            // Don't buy
-            skip = solve(prices, idx + 1, 1, cap);
-        }
-        else {
-            // Sell
-            take = prices[idx] + solve(prices, idx + 1, 1, cap - 1);
-
-            // Don't sell
-            skip = solve(prices, idx + 1, 0, cap);
+        if(dp[idx][buy][maxLimit]!=-1){
+            return dp[idx][buy][maxLimit];
         }
 
-        return dp[idx][buy][cap] = max(take, skip);
+        int take,skip;
+        if(buy){
+            take=-prices[idx]+solve(prices,idx+1,0,maxLimit);
+            skip=solve(prices,idx+1,1,maxLimit);
+        }
+        else{
+            take=prices[idx]+solve(prices,idx+1,1,maxLimit-1);
+            skip=solve(prices,idx+1,0,maxLimit);
+        }
+
+        return dp[idx][buy][maxLimit]=max(take,skip);
+
     }
-
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-
-        memset(dp, -1, sizeof(dp));
-
-        return solve(prices, 0, 1, 1);
+        n=prices.size();
+        // 1 means we are buying 
+        memset(dp,-1,sizeof(dp));
+        return solve(prices,0,1,1);
+        
     }
 };
