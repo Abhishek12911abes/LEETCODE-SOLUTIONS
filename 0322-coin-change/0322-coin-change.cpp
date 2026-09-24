a484a55d692c047cf597ba07742eb6e79cdf7d80
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-24 19:17:41 +0530

    Time: 28 ms (59.4%), Space: 13.7 MB (97.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int n;
-    int dp[13][10001];
-    typedef long long ll;
-    ll solve(vector<int>& coins , int amount , int idx){
+    int dp[10001][13];
+    int solve(vector<int>& coins , int amount , int idx){
         if(amount==0){
             return 0;
         }
-        if(idx>=coins.size()){
+        if(amount<0 || idx>=n){
             return INT_MAX;
         }
-        if(amount<0){
-            return INT_MAX;
+        if(dp[amount][idx]!=-1){
+            return dp[amount][idx];
         }
-        if(dp[idx][amount]!=-1){
-            return dp[idx][amount];
-        }
-        ll take=1+solve(coins,amount-coins[idx],idx);
-        ll skip=solve(coins,amount,idx+1);
+        int take=solve(coins,amount-coins[idx],idx);
+        if(take != INT_MAX)
+            take += 1;
+        int skip=solve(coins,amount,idx+1);
 
-        return dp[idx][amount]=min(take,skip);
+        return dp[amount][idx]=min(take,skip);
     }
     int coinChange(vector<int>& coins, int amount) {
         n=coins.size();
+
+
         memset(dp,-1,sizeof(dp));
+
         int ans=solve(coins,amount,0);
+
         return ans==INT_MAX?-1:ans;
         
     }
