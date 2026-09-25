09a23c10d0400c02a4703216d28a36c84f21bae7
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-25 16:30:03 +0530

    Time: 3 ms (45.37%), Space: 17.5 MB (30.39%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
+    int n;
+    int solve(vector<int>& nums , unordered_map<int,int>& mp , int elem , int maxElem, vector<int>&dp){
+        if(elem>maxElem){
+            return 0;
+        }
+
+        if(dp[elem]!=-1){
+            return dp[elem]; 
+        }
+        int take=0,skip=0;
+
+        take=mp[elem]*elem+solve(nums,mp,elem+2,maxElem,dp);
+
+        skip=solve(nums,mp,elem+1,maxElem,dp);
+
+        return dp[elem]=max(take,skip);
+    }
     int deleteAndEarn(vector<int>& nums) {
+        n=nums.size();
+        unordered_map<int,int>mp;
 
-        int maxi = *max_element(nums.begin(), nums.end());
 
-        vector<int> points(maxi + 1, 0);
 
-        // Calculate total points for every number
-        for(int x : nums) {
-            points[x] += x;
+        sort(begin(nums),end(nums));
+
+        int maxElem=nums.back();
+
+        for(int num : nums){
+            mp[num]++;
         }
 
-        vector<int> dp(maxi + 1, 0);
+        vector<int>dp(maxElem+1,-1);
 
-        dp[0] = 0;
-
-        if(maxi >= 1)
-            dp[1] = points[1];
-
-        for(int i = 2; i <= maxi; i++) {
-
-            // Don't take i
-            int skip = dp[i - 1];
-
-            // Take i
-            int take = points[i] + dp[i - 2];
-
-            dp[i] = max(take, skip);
-        }
-
-        return dp[maxi];
+        return solve(nums,mp,nums[0],maxElem,dp);
     }
 };
