f31bfc3b8c9b779bcaf3c5415068e298d18e6884
Abhishek Singh <168820932+Abhishek12911abes@users.noreply.github.com>
2026-09-25 12:30:19 +0530

    Time: 3 ms (25.15%), Space: 13.4 MB (57.17%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,84 +1,84 @@
-//Approach-1 (Recursion + Memoized)  [Time : O(max_day)] [Space : O(n)] //n = size of days vector
-class Solution {
-public:
-    int t[366];
-    int memoized(vector<int>& days, vector<int>& costs, int& n, int idx) {
-        if(idx >= n)
-            return 0; //you can't travel, so no cost
-                
-        if(t[idx] != -1)
-            return t[idx];
-        
-        //if i take 1-day pass at idx
-        int cost_1 = costs[0] + memoized(days, costs, n, idx+1);
-        
-        
-        //if i take 7-day pass at idx
-        int j          = idx;
-        int maxDays=days[idx]+7;
-        while(j < n && days[j] < maxDays) {
-            j++;
-        }
-        int cost_7 = costs[1] + memoized(days, costs, n, j);
-        
-        
-        //if i take 30-day pass at idx
-        j    = idx;
-        maxDays=days[idx]+30;
-        while(j < n && days[j] < maxDays) {
-            j++;
-        }
-        int cost_30 = costs[2] + memoized(days, costs, n, j);
-        
-        
-        return t[idx] = min({cost_1, cost_7, cost_30});
-    }
-    
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        memset(t, -1, sizeof(t));
-        int n = days.size();
-        return memoized(days, costs, n, 0);
-    }
-};
-
-
-// //Approach-2 (Bottom Up DP) [Time : O(max_day)] [Space : O(n)] //n = size of days vector
+// //Approach-1 (Recursion + Memoized)  [Time : O(max_day)] [Space : O(n)] //n = size of days vector
 // class Solution {
-// public:    
-//     int bottomUp(vector<int>& days, vector<int>& costs) {
-//         unordered_set<int> st(begin(days), end(days));
-//         int last_day = days.back();
+// public:
+//     int t[366];
+//     int memoized(vector<int>& days, vector<int>& costs, int& n, int idx) {
+//         if(idx >= n)
+//             return 0; //you can't travel, so no cost
+                
+//         if(t[idx] != -1)
+//             return t[idx];
         
-//         vector<int> t(last_day+1, 0);
-//         //t[i] = min cost to reach till day i of your travel plan
-//         t[0] = 0; //since, on day 0, we spend cost = 0
+//         //if i take 1-day pass at idx
+//         int cost_1 = costs[0] + memoized(days, costs, n, idx+1);
         
-//         for(int i = 1; i<=last_day; i++) {
-//             if(st.find(i) == st.end()) {
-//                 t[i] = t[i-1]; //skip
-//                 continue;
-//             }
-//             //you need to filter those days from 1 to 30 which are not in days vector
-//             t[i] = INT_MAX;
-            
-//             //I come to i either by taking a 1-day pass from jth day
-//             int day_1_pass = t[max(0, i-1)] + costs[0];
-
-//             //I come to i either by taking a 7-day pass from jth day
-//             int day_7_pass = t[max(0,i-7)] + costs[1];
-
-//             //I come to i either by taking a 30-day pass from jth day
-//             int day_30_pass = t[max(0, i-30)] + costs[2];
-            
-//             t[i] = min({day_1_pass, day_7_pass, day_30_pass});
+        
+//         //if i take 7-day pass at idx
+//         int j          = idx;
+//         int maxDays=days[idx]+7;
+//         while(j < n && days[j] < maxDays) {
+//             j++;
 //         }
+//         int cost_7 = costs[1] + memoized(days, costs, n, j);
         
-//         return t[last_day]; //minimum cost to travel till my last planned day
+        
+//         //if i take 30-day pass at idx
+//         j    = idx;
+//         maxDays=days[idx]+30;
+//         while(j < n && days[j] < maxDays) {
+//             j++;
+//         }
+//         int cost_30 = costs[2] + memoized(days, costs, n, j);
+        
+        
+//         return t[idx] = min({cost_1, cost_7, cost_30});
 //     }
     
 //     int mincostTickets(vector<int>& days, vector<int>& costs) {
-//         return bottomUp(days, costs);     
+//         memset(t, -1, sizeof(t));
+//         int n = days.size();
+//         return memoized(days, costs, n, 0);
 //     }
 // };
+
+
+// //Approach-2 (Bottom Up DP) [Time : O(max_day)] [Space : O(n)] //n = size of days vector
+class Solution {
+public:    
+    int bottomUp(vector<int>& days, vector<int>& costs) {
+        unordered_set<int> st(begin(days), end(days));
+        int last_day = days.back();
+        
+        vector<int> t(last_day+1, 0);
+        //t[i] = min cost to reach till day i of your travel plan
+        t[0] = 0; //since, on day 0, we spend cost = 0
+        
+        for(int i = 1; i<=last_day; i++) {
+            if(st.find(i) == st.end()) {
+                t[i] = t[i-1]; //skip
+                continue;
+            }
+            //you need to filter those days from 1 to 30 which are not in days vector
+            t[i] = INT_MAX;
+            
+            //I come to i either by taking a 1-day pass from jth day
+            int day_1_pass = t[max(0, i-1)] + costs[0];
+
+            //I come to i either by taking a 7-day pass from jth day
+            int day_7_pass = t[max(0,i-7)] + costs[1];
+
+            //I come to i either by taking a 30-day pass from jth day
+            int day_30_pass = t[max(0, i-30)] + costs[2];
+            
+            t[i] = min({day_1_pass, day_7_pass, day_30_pass});
+        }
+        
+        return t[last_day]; //minimum cost to travel till my last planned day
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        return bottomUp(days, costs);     
+    }
+};
 
 
